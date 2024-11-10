378b04278d693d72d2775fe91a8f8fbff3a08922
Mari Teini <t3tema00@students.oamk.fi>
2024-11-10 11:08:37 +0200

    h1 valmis

--- h1/main.cpp
+++ h1/main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 
-using namespace std;
+int calcSum(int a, int b) {
+    return a + b;  // Returns the sum of a and b
+}
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int calcDiv(int a, int b) {
+    if (b != 0) {
+        return a / b;  // Returns the result of a divided by b
+    } else {
+        std::cout << "Error: Division by zero!" << std::endl;
+        return 0;  // Return 0 if division by zero occurs
+    }
+}
+
+int main() {
+    int a, b;
+
+
+
+    std::cout << "Give the first number: ";
+    std::cin >> a;
+
+
+    std::cout << "Give the second number: ";
+    std::cin >> b;
+
+
+    int sum = calcSum(a, b);
+    std::cout << "The sum of " << a << " and " << b << " is " << sum << std::endl;
+
+
+    if (b != 0) {
+        int division = calcDiv(a, b);
+        std::cout << "The division of " << a << " by " << b << " is " << division << std::endl;
+    }
+
     return 0;
 }
