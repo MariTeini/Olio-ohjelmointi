0a7ed7d1094bfd0db4a5db5e38406e43cf0fec10
Mari Teini <t3tema00@students.oamk.fi>
2024-12-14 16:08:47 +0200

    tehtävä 6b valmis

--- h6a/main.cpp
+++ h6a/main.cpp
@@ -6,15 +6,35 @@
 class MyClass : public QObject {
     Q_OBJECT
 
+
 public:
-    explicit MyClass(QObject* parent = nullptr);
-    void raiseMySignal();
+    MyClass(QObject* parent = nullptr) : QObject(parent) {}
+
+
+    void raiseMySignal() {
+        emit mySignal();
+    }
 
 signals:
     void mySignal();
 
 public slots:
     void mySlot();
-
-#endif //MYCLASS_H
+    qDebug() << "mySignal signaal!";
 };
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+MyClass myObject;
+
+QObject::connect(&myObject, &MyClass::mySignal, &myObject, &MyClass::mySlot);
+
+ myObject.raiseMySignal();
+
+#endif //MYCLASS_H  MyClass myObject;
+
+ return 0;
+
+}
---
+++ h6b/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+#include "exampleclass.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+
+ExampleClass example;
+
+
+example.startToWait();
+
+    return a.exec();
+}
